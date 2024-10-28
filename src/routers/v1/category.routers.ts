95b11ffe19edc38e5ea8a0138e62609ee9caac56
denbahrul <ulum0326@gmail.com>
2024-10-28 15:15:44 +0700

    feat : authorization middleware

--- src/routers/v1/category.routers.ts
+++ src/routers/v1/category.routers.ts
@@ -1,10 +1,11 @@
 import categoryControllers from "@/controllers/category.controllers";
 import { authentication } from "@/middlewares/authentication";
+import authorization from "@/middlewares/authorization";
 import { Router } from "express";
 
 export const categoryRoutes = Router();
 
 categoryRoutes.get("/", categoryControllers.findAll);
-categoryRoutes.post("/create", authentication, categoryControllers.create);
-categoryRoutes.patch("/update/:id", authentication, categoryControllers.update);
-categoryRoutes.delete("/delete/:id", authentication, categoryControllers.delete);
+categoryRoutes.post("/create", authentication, authorization("ADMIN"), categoryControllers.create);
+categoryRoutes.patch("/update/:id", authentication, authorization("ADMIN"), categoryControllers.update);
+categoryRoutes.delete("/delete/:id", authentication, authorization("ADMIN"), categoryControllers.delete);
--- src/routers/v1/product.routers.ts
+++ src/routers/v1/product.routers.ts
@@ -1,6 +1,7 @@
 import categoryControllers from "@/controllers/category.controllers";
 import productControllers from "@/controllers/product.controllers";
 import { authentication } from "@/middlewares/authentication";
+import authorization from "@/middlewares/authorization";
 import upload from "@/middlewares/upload-file";
 import { Router } from "express";
 
@@ -8,6 +9,6 @@ export const productRoutes = Router();
 
 productRoutes.get("/", productControllers.findAll);
 productRoutes.get("/:id", productControllers.findById);
-productRoutes.post("/create", authentication, upload.array("productImage", 4), productControllers.create);
-productRoutes.patch("/update/:id", authentication, productControllers.update);
-productRoutes.delete("/delete/:id", authentication, productControllers.delete);
+productRoutes.post("/create", authentication, authorization("ADMIN"), upload.array("productImage", 4), productControllers.create);
+productRoutes.patch("/update/:id", authentication, authorization("ADMIN"), productControllers.update);
+productRoutes.delete("/delete/:id", authentication, authorization("ADMIN"), productControllers.delete);
